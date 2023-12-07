5343816c69fd9f0fe36a86f7371afa169cb0e86c
CerealAxis <2741798712@qq.com>
2023-12-07 21:56:56 +0800

    更新文件

--- 【入门2】分支结构/P1888 三角函数.cpp
+++ 【入门2】分支结构/P1888 三角函数.cpp
@@ -1,43 +1,31 @@
 #include <stdio.h>
-#include <math.h>
 
-int gcd(int a, int b) {
-	if (b == 0)
-		return a;
-	return gcd(b, a % b);
-}
-
-void simplifyFraction(int *numerator, int *denominator) {
-	int divisor = gcd(*numerator, *denominator);
-	*numerator /= divisor;
-	*denominator /= divisor;
-}
-
-void calculateSineFraction(int a, int b, int c, int *sine_numerator, int *sine_denominator) {
-	double semi_perimeter = (a + b + c) / 2.0;
-	double area = sqrt(semi_perimeter * (semi_perimeter - a) * (semi_perimeter - b) * (semi_perimeter - c));
-	double sine = (2 * area) / (b * c);
-
-	int numerator = (int)(sine * c);
-	int denominator = b;
-
-	simplifyFraction(&numerator, &denominator);
-
-	*sine_numerator = numerator;
-	*sine_denominator = denominator;
-}
-
-int main() {
-	int a, b, c;
-	scanf("%d %d %d", &a, &b, &c);
-
-	if (a <= 0 || b <= 0 || c <= 0 || (a + b <= c) || (a + c <= b) || (b + c <= a)) {
-		printf("\n");
-	} else {
-		int sine_numerator, sine_denominator;
-		calculateSineFraction(a, b, c, &sine_numerator, &sine_denominator);
-		printf("%d/%d\n", sine_numerator, sine_denominator);
+int main () {
+	int i[3], r, j, temp, c, a, b, k, l;
+	int gcd (int a, int b);
+	scanf("%d %d %d", &i[0], &i[1], &i[2]);
+	for (r = 0; r < 3; r++) {
+		for (j = 0; j < 3; j++) {
+			if (i[r] > i[j]) {
+				temp = i[j];
+				i[j] = i[r];
+				i[r] = temp;
+			}
+		}
 	}
-
+//	printf("%d %d %d\n", i[0], i[1], i[2]);
+	c = gcd (i[0], i[2]);
+//	printf("%d\n", c);
+	k = i[2] / c;
+	l = i[0] / c;
+	printf("%d/%d", k, l);
 	return 0;
+}
+
+int gcd (int a, int b) {
+	if (b == 0) {
+		return a;
+	} else {
+		return gcd(b, a % b);
+	}
 }
